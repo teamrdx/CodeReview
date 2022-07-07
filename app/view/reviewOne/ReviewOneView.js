d3b89ae2709b4c85e6aebf0ccc9a8e14d63ba238
Torsten <td@m-gd.com>
2022-07-07 11:17:57 +0200

    #third_review

--- app/view/reviewOne/ReviewOneView.js
+++ app/view/reviewOne/ReviewOneView.js
@@ -1,50 +1,60 @@
 // @hint all strings go with single quotes in ExtJS
 Ext.define('CodeReview.view.reviewOne.ReviewOneView', {
-  extend: 'Ext.form.Panel',
-  xtype: 'reviewOne',
+    extend: 'Ext.form.Panel',
+    xtype: 'reviewOne',
 
-  requires: [
-    'CodeReview.view.reviewOne.ReviewOneViewModel',
-    'CodeReview.view.reviewOne.ReviewOneViewController'
-  ],
+    requires: [
+        'CodeReview.view.reviewOne.ReviewOneViewModel',
+        'CodeReview.view.reviewOne.ReviewOneViewController'
+    ],
 
-  viewModel: { type: 'reviewOne' },
-  controller: 'reviewOne',
+    viewModel: {type: 'reviewOne'},
+    controller: 'reviewOne',
 
-  bind: { title: '{form.title}' },
+    bind: {title: '{form.title}'},
 
-  layout: 'vbox',
-  defaults: { width: 600 },
-  items: [
-    {
-      xtype: 'textfield',
-      bind: {
-        fieldLabel: '{form.fieldLabel}',
-        emptyText: '{form.defaultMessage}'
-      },
-      labelWidth: 150,
-      name: 'whatYouThink',
-      margin: '15 0',
-      allowBlank: false
-    }
-  ],
+    layout: 'vbox',
+    defaults: {width: 600},
+    items: [
+        {
+            // @hint the important one for fields is `name`
+            //     first goes the xtype, second the important config (id, title, text, name)
+            //     then custom configs (self made configs)
+            //     then configs (string, int, boolean)
+            //     then configs (object) single line
+            //     then configs (object) multi line
+            //     last listeners
+            xtype: 'textfield',
+            name: 'whatYouThink',
 
-  buttons: [
-    {
-      bind: { text: '{language.resetButtonText}' },
-      listeners: {
-        click: 'onClickButtonReset'
-      }
-    },
-    {
-      bind: { text: '{language.defaultButtonText}' },
-      formBind: true,
-      disabled: true,
-      listeners: {
-        click: 'onClickButtonSubmit'
-      }
-    }
-  ],
+            labelWidth: 150,
+            margin: '15 0',
+            allowBlank: false,
 
-  listeners: {}
+            value: 'foo',
+            bind: {
+                value: '{form.whatYouThink}',
+                fieldLabel: '{form.fieldLabel}',
+                emptyText: '{form.defaultMessage}'
+            }
+        }
+    ],
+
+    buttons: [
+        {
+            bind: {text: '{language.resetButtonText}'},
+            listeners: {
+                click: 'onClickButtonReset'
+            }
+        },
+        {
+            formBind: true,
+            disabled: true,
+            bind: {text: '{language.defaultButtonText}'},
+            listeners: {
+                click: 'onClickButtonSubmit'
+            }
+        }
+    ]
+    // @hint no need for an empty listeners object
 });
--- app/view/reviewOne/ReviewOneViewController.js
+++ app/view/reviewOne/ReviewOneViewController.js
@@ -1,40 +1,40 @@
 Ext.define('CodeReview.view.reviewOne.ReviewOneViewController', {
-  extend: 'Ext.app.ViewController',
-  alias: 'controller.reviewOne',
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.reviewOne',
 
-  uses: ['Codereview.view.reviewOne.ReviewOnePopupWindow'],
+    uses: ['Codereview.view.reviewOne.ReviewOnePopupWindow'],
 
-  onClickButtonSubmit: function (btn) {
-    // Getting the ViewModel and storing it in the variable vm
-    var view = this.getView(),
-      vm = view.getViewModel();
+    onClickButtonSubmit: function (btn) {
+        // Getting the ViewModel and storing it in the variable vm
+        const view = this.getView(),
+            // @hint use this.getViewModel in the controller
+            vm = this.getViewModel(),
+            // Getting the Values of the form
+            whatYouThink = view.getForm().getValues().whatYouThink;
 
-    // Getting the Values of the form
-    var { whatYouThink } = view.getForm().getValues();
+        console.log(whatYouThink);
 
-    // Setting the new values in the ViewModel
-    vm.set('language.defaultButtonText', 'Thanks');
-    vm.set('form.defaultMessage', whatYouThink);
+        // Setting the new values in the ViewModel
+        vm.set('language.defaultButtonText', 'Thanks');
 
-    // Setting new button Icon
-    btn.setIconCls('fa fa-thumbs-up');
+        // Setting new button Icon
+        btn.setIconCls('fa fa-thumbs-up');
 
-    // Creating the popup with the parent viewmodel and displaying it in the view
-    Ext.create('Codereview.view.reviewOne.ReviewOnePopupWindow', {
-      viewModel: { parent: vm }
-    }).show();
-  },
+        // Creating the popup with the parent viewmodel and displaying it in the view
+        Ext.create('Codereview.view.reviewOne.ReviewOnePopupWindow', {
+            viewModel: {parent: vm}
+        });
+    },
 
-  onClickButtonReset: function (btn) {
-    // Getting the view and ViewModel
-    var view = this.getView(),
-      vm = view.getViewModel();
+    onClickButtonReset: function (btn) {
+        // Getting the view and ViewModel
+        var view = this.getView(),
+            vm = view.getViewModel();
 
-    // Resetting the value entered in the form
-    view.getForm().reset();
+        // Resetting the value entered in the form
+        view.getForm().reset();
 
-    // Resetting the old values in the ViewModel
-    vm.set('language.defaultButtonText', 'Click & Find');
-    vm.set('form.defaultMessage', 'Foo');
-  }
+        // Resetting the old values in the ViewModel
+        vm.set('language.defaultButtonText', 'Click & Find');
+    }
 });
